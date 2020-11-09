401c3a3e92cec30f0230d2368f8a5800d8a19b40
souzet tadros <souzettadros@gmail.com>
2020-11-09 11:15:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration:
+            string command;
+            // Greetings:
+            Console.WriteLine("Hello and welcome todo list");
+            Console.WriteLine("To quit type 'quit', for help type 'help'");
+            // REPl (do-while-loop)
+            do
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                //    Om commandot är load: läs filen
+                if (command == "quit")
+                {
+                    Console.WriteLine("Bye!");
+                }
+                else
+                {
+                    Console.WriteLine("unknown command: {0}", command);
+                }
+            }
+            while (command != "quit");
+        }
+    }
+    
+}
